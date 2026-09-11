abe920528ade06f999533622c26cfafda6ee957d
Fault <soyfault@gmail.com>
2026-09-10 21:54:27 -0600

    Rollback as this strings don't work.
    Will change later mayble but I don't know anyone that uses keybinds lol

--- DivaniKeybinds.cs
+++ DivaniKeybinds.cs
@@ -7,11 +7,9 @@ namespace DivaniMods;
 [RegisterCustomKeybinds]
 public static class DivaniKeybinds
 {
-    public static MiraKeybind TeleportPortal1 { get; } = new(
-        MiraLocaleManager.Get("DivaniMods.Keybind.TeleportPortal1"),
-        KeyboardKeyCode.Alpha1);
+    public static MiraKeybind TeleportPortal1 { get; } =
+        new("Teleport To Portal 1", KeyboardKeyCode.Alpha1);
 
-    public static MiraKeybind TeleportPortal2 { get; } = new(
-        MiraLocaleManager.Get("DivaniMods.Keybind.TeleportPortal2"),
-        KeyboardKeyCode.Alpha2);
+    public static MiraKeybind TeleportPortal2 { get; } =
+        new("Teleport To Portal 2", KeyboardKeyCode.Alpha2);
 }
